e0960bf6ce179a5719cd7a06a4b9422a182da6a1
Azharuddin Ali <azharuddinali017@gmail.com>
2026-03-02 00:27:55 +0530

    Added badge component as well some home page content added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div><h1>Share What You&apos;ve Built, Discover What&apos;s Launching</h1></div>
+    <div>
+      <span>Join thousands of creators sharing their work</span>
+
+      <h1>Share What You&apos;ve Built, Discover What&apos;s Launching</h1>
+
+      <p>A community platform for creators to showcase their apps, AI tools,
+            SaaS products, and creative projects. Authentic launches, real
+            builders, genuine feedback.</p>
+      
+      <Button>Share Your Project</Button>
+      <Button>Explore projects</Button>
+    </div>
   );
 }

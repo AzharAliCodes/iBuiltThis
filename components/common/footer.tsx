e97f66412b2c79d184af850fb3c45233f2592eaf
Azharuddin Ali <azharuddinali017@gmail.com>
2026-03-02 22:01:45 +0530

    Final Next js app is Ready

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="border-t bg-muted/20 py-12">
-      <div className="wrapper">iBuiltThis Inc. All rights reserved.</div>
-    </footer>
+  <div className="max-w-7xl mx-auto px-4 text-center">
+    <p className="text-sm text-muted-foreground">
+      © {new Date().getFullYear()} iBuiltThis by Azharuddin Ali. All rights reserved.
+    </p>
+  </div>
+</footer>
   );
 }
